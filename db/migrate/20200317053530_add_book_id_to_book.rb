70e9b956f5ccdfc5b28f00615bf5a76773aa84a2
EC2 Default User <ec2-user@ip-172-31-40-31.ap-northeast-1.compute.internal>
2020-05-04 15:10:27 +0000

    fix: db config

--- db/migrate/20200317053530_add_book_id_to_book.rb
+++ db/migrate/20200317053530_add_book_id_to_book.rb
@@ -1,5 +1,5 @@
 class AddBookIdToBook < ActiveRecord::Migration[5.2]
-  def change
-    add_column :books, :book_id, :integer
-  end
+  # def change
+  #   add_column :books, :book_id, :integer
+  # end
 end
