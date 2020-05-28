e873e6003636f312aae5d504d7891badaefd0ffb
mushakora <mushakora89br@yahoo.co.jp>
2020-05-28 23:55:49 +0900

    [add]bookテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_133300) do
+ActiveRecord::Schema.define(version: 2020_05_28_145433) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_133300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "book_id"
+    t.datetime "date"
   end
 
   create_table "direct_messages", force: :cascade do |t|
